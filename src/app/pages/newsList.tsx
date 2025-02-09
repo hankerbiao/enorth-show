bc6d72431b84ee4377e8dccaa8fea71bbfb963b4
libiao <3300434146@qq.com>
2025-02-09 19:03:50 +0800

    修改默认地址

--- src/app/pages/newsList.tsx
+++ src/app/pages/newsList.tsx
@@ -53,7 +53,7 @@ const NewsList: React.FC = () => {
     const fetchNews = async (params: NewsParams = {}) => {
         setLoading(true);
         try {
-            const response = await axios.get<{ items: NewsItem[], total: number }>('http://192.168.3.113:8001/api/v1/news', {
+            const response = await axios.get<{ items: NewsItem[], total: number }>('http://113.44.137.165:8001/api/v1/news', {
                 params: params
             });
             setNews(response.data.items);
